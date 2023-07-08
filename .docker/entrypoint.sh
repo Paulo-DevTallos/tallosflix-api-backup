ff76ab01dcbef9eb67fda6fd115bd98d03aeb24b
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-07-08 14:37:16 -0300

    refactor: change observer script into entrypoint

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-path=/home/paulo-developer/developer/paulo-development/projects/submodulo-talloflix
-folder=api-tallosflix
-dockerfile_prod=Dockerfile.prod
-dockerfile_dev=Dockerfile
-
 npm install
 
-if [ $directory/$folder/$dockerfile_prod ];
-then
-    npm run start
-else
-    npm run start:dev
-fi
+npm run start:dev
+
+# path=/home/paulo-developer/developer/paulo-development/projects/submodulo-talloflix
+# folder=api-tallosflix
+# dockerfile_prod=Dockerfile.prod
+# dockerfile_dev=Dockerfile
+
+# npm install
+
+# if [ $directory/$folder/$dockerfile_prod ];
+# then
+#     npm run start
+# else
+#     npm run start:dev
+# fi
