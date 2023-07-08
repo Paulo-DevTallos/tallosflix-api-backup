a9a045ed6a744adf905c4ef6506bb4517bfefc3e
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-07-08 14:37:42 -0300

    feat: add user communication service e controller

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  getUSers() {
+    const user = [
+      {
+        name: 'Ana Paula',
+        email: 'anapa@teste.com.br',
+      },
+      {
+        name: 'Fernanda',
+        email: 'fernanda@teste.com.br'
+      }
+    ]
+
+    return user
+  }
+}
